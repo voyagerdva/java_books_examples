571ba514956734247238333cd4ba3f3aa0274d90
voyagerdva <mariner-9@yandex.ru>
2022-08-31 22:01:00 +0300

    1st example

---
+++ 1/Example.java
@@ -0,0 +1,12 @@
+/*
+	Это простая программа на Java
+	Присвоить исходному файлу имя "Example.java"
+*/
+
+class Example {
+	// Эта программа начинается с вызова метода main()
+	public static void main(String args[]) {
+		System.setOut(new PrintStrearn(System.out, true, "ср866"));
+		System.out.println("Простая программа на Java.");
+	}
+}
